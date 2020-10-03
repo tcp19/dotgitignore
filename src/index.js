3cf8a4f7eb42cb408eaf306e0dd2ac58584556e9
mohamilr <ibrahimdamy7@gmail.com>
2020-10-03 15:58:41 +0100

    add inquirer-auto-complete-search

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ const { writeFile, readdir, readFile } = require("fs").promises;
 const path = require("path");
 const inquirer = require("inquirer");
 const chalk = require("chalk");
+const fuzzy = require('fuzzy');
+const _ = require('lodash');
+// register autocomplete search
+inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
 
 /**
  * get all files in the ignore files directory
@@ -26,21 +30,43 @@ const configFolderPath = path.resolve(__dirname, "ignorefiles");
   console.log(chalk.blue("dotgitignore"));
   console.log(chalk.blue("Easily ignore environment files on your projects"));
 
+
+  // search fuction
+  function ignoreFiles(answer, input) {
+    input = input || '';
+
+    return new Promise(function (resolve) {
+      setTimeout(function () {
+        const ignoreNames = Object.keys(configFiles)
+        var fuzzyResult = fuzzy.filter(input, ignoreNames);
+        resolve(
+          fuzzyResult.map(function (el) {
+            return el.original;
+          })
+        );
+      }, _.random(30, 500));
+    });
+  }
+
   //prompt user
   await inquirer
     .prompt([
       {
         name: "ignore",
-        type: "list",
-        message: "What project file do you wish to ignore",
-        choices: Object.keys(configFiles),
+        type: "autocomplete",
+        message: "What project file do you wish to ignore. search:",
+        source: ignoreFiles,
+        searchText: 'Searching...',
+        emptyText: 'Nothing found!',
+        pageSize: 4,
       },
     ])
-    .then(async ({ ignore }) => {
+    .then(async ({ignore}) => {
       // read file based on chosen value
       const config = await readFile(configFiles[ignore]).catch((err) =>
         console.log(chalk.red(err))
       );
+      console.log(config)
       // write fiile to root directory
       const gitignore = path.join(process.cwd(), ".gitignore");
       await writeFile(gitignore, config).catch((err) => {
